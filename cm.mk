529d54961a2ad5c2ecd8c52266dba51b13cd1ec1
Shareef Ali <shareefalis@gmail.com>
2012-08-21 21:21:39 -0500

    d2usc: fixes for  compile and misc changes

--- cm.mk
+++ cm.mk
@@ -9,7 +9,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += ro.build.description=d2usc-user 4.0.4 IMM76D R530UVXALG1 release-keys ro.build.fingerprint=samsung/d2usc/d2usc:4.0.4/IMM76D/R530UVXALG1:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2usc TARGET_DEVICE=d2usc BUILD_FINGERPRINT="d2usc-user 4.0.4 IMM76D R530UVXALG1 release-keys" PRIVATE_BUILD_DESC="samsung/d2usc/d2usc:4.0.4/IMM76D/R530UVXALG1:user/release-keys"
+
 TARGET_BOOTANIMATION_NAME := vertical-720x1280
 
 PRODUCT_NAME := cm_d2usc
