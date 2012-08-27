8ccc97bdcc16c714ce0108175770b2dae2443ac9
Shareef Ali <shareefalis@gmail.com>
2012-08-26 23:04:06 -0500

    d2: move to d2 common
    Change-Id: If9f216cdd64d0956979f48ea7bf8bb5f13ee1b88

--- device.mk
+++ device.mk
@@ -19,6 +19,8 @@ $(call inherit-product-if-exists, vendor/samsung/d2usc/d2usc-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2usc/overlay
+## common overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-cdma
 
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
