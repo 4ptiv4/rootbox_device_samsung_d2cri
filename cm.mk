5f10563022bbf74af92f6073ad8f2f14ee667b6c
Shareef Ali <shareefalis@cyanogenmod.org>
2012-12-24 20:47:04 -0600

    fix build fingerprint

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2cri TARGET_DEVICE=d2cri BUILD_FINGERPRINT="d2cri-user 4.0.4 IMM76D R530UVXALG1 release-keys" PRIVATE_BUILD_DESC="samsung/d2cri/d2cri:4.0.4/IMM76D/R530UVXALG1:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2cri TARGET_DEVICE=d2cri BUILD_FINGERPRINT="d2cri-user 4.0.4 IMM76D R530CVXALG1 release-keys" PRIVATE_BUILD_DESC="samsung/d2cri/d2cri:4.0.4/IMM76D/R530CVXALG1:user/release-keys"
 
 PRODUCT_NAME := cm_d2cri
 PRODUCT_DEVICE := d2cri
